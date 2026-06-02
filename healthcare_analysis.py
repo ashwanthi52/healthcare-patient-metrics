620ab7769188e3ce2c868e27b97bc358aad81daf
ashwanthi52 <ashwanthi52@gmail.com>
2026-06-02 16:20:21 +0530

    Update healthcare_analysis.py

--- healthcare_analysis.py
+++ healthcare_analysis.py
@@ -1,5 +1,13 @@
 import pandas as pd
-
-print("Healthcare Patient Metrics Project")
+df=pd.read_csv("healthcare.csv")
+print("Healthcare Patient Metrics Project analysis")
+print("\ndataset:")
+print(df.head())
+print("\naverage age:")
+print(df["age"].mean())
+print("\naverage treatment cost:")
+print(df["treatment_cost"].mean())
+print("\ndisease count:")
+print(df["disease"].value_counts())
 
 # Dataset will be added later
